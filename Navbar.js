01bf563bb665cf035aa1090857142f7517fac046
ango06 <143280698+ango06@users.noreply.github.com>
2025-08-13 19:14:30 -0400

    Navbar component

---
+++ Navbar.js
@@ -0,0 +1,28 @@
+import logo from "../src/ganjiLogo.png";
+import './App.css';
+
+function Navbar() {
+  return (
+    <div>
+      <nav id="navbar">
+        <ul>
+          <li>
+            <a href="#default" style={{ padding: 0 }}>
+              <img id="logo" src={logo} alt="Ganji logo" />
+            </a>
+          </li>
+          <div>
+            <li><a href="#about">About</a></li>
+            <li><a href="#officers">Officers</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#workshops">Workshops</a></li>
+            <li><a href="#performances">Performances</a></li>
+            <li><a href="#archives">Archives</a></li>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
